dd8ef3b5e26e1ce4319654ef1d40f71da7f68fe1
Edson Jr <eds_jr@hotmail.com>
2019-06-24 20:04:03 -0400

    Variaveis booleanas
    Signed-off-by: Edson Jr <eds_jr@hotmail.com>

---
+++ Intro/Intellij/variaveis_booleanas/bin/main.dart
@@ -0,0 +1,30 @@
+import 'package:variaveis_booleanas/variaveis_booleanas.dart' as variaveis_booleanas;
+
+main(List<String> arguments) {
+
+  /*Variaveis booleanas em dart
+  *
+  * true = true
+  * false = false
+  *
+  * */
+
+  var t = true;
+  var f = false;
+
+  //outra maneira de se utilizar o booleano no dart
+  bool verdadeiro = true;
+  bool falso = false;
+
+
+
+  print('Os valores de t e f sao, respectivamente: ${t} e ${f}');
+  print('Verdadeiro: ${verdadeiro}');
+  print('falso: ${falso}');
+
+  
+
+
+  //Booleano com operador ternario
+
+}
---
+++ Intro/Intellij/variaveis_booleanas/test/variaveis_booleanas_test.dart
@@ -0,0 +1,8 @@
+import 'package:variaveis_booleanas/variaveis_booleanas.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
