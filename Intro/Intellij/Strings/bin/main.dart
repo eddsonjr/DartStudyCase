0f2ffef2aa9fa822c365615a1306047eee9d4d07
Edson Jr <eds_jr@hotmail.com>
2019-06-24 20:42:59 -0400

    Strings.
    Signed-off-by: Edson Jr <eds_jr@hotmail.com>

--- Intro/Intellij/Strings/bin/main.dart
+++ Intro/Intellij/Strings/bin/main.dart
@@ -27,5 +27,44 @@ main(List<String> arguments) {
   print('Tipo splitedName: ${splitedName.runtimeType}'); //retorna o tipo da var
 
 
+  //Retirando uma substring com base na posicao dos caracteres
+  String nickname = nome.substring(0,3);
+  print('Nickname: $nickname');
+
+  //Outra forma
+  String nickname2 = splitedName[0].substring(0,3);
+  print('Nickname: $nickname2');
+  
+  //Acessando os caracteres de uma string
+  String soup = 'Sopa de Letrinhas';
+  int index = soup.indexOf(' de ');
+  print('Index: $index');
+  print('A sopa é: ${soup.substring(index).trim()}');
+
+  //Tamanho de uma string
+  print('Tamanho da sopa de letrinhas: ${soup.length}');
+
+
+  //procurando substrings (verificando se existe ou nao)
+  print('Contem #: ${soup.contains('#')}');
+  print('Contem t: ${soup.contains('t')}');
+
+  //Comparando strings
+  var s1 = 'Shirley';
+  var s2 = 'shirley';
+  var s3 = 'Shirley';
+  print('Shirley = shirley: ${s1 == s2}');
+  print('Shirley = Shirley: ${s1 == s3}');
+
+
+  //Colocando uma variavel toda em minusculo e maiusculo
+  print('Minusculo: ${'SHIRLEY'.toLowerCase()}');
+  print('Maiusculo: ${'shirley'.toUpperCase()}');
+
+  //Substrituindo uma string por outra
+  print('Sopa de: ${soup.replaceAll('Letrinhas', 'Macarrao')}');
+  //procura a substring 'Letrinhas' e substitui por 'Macarrao'
+
+
   
 }
