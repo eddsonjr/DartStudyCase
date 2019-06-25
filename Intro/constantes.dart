d3a8be8c80def6ce8192c18dc00b26c2da08ed9a
Edson Jr <eds_jr@hotmail.com>
2019-06-24 20:49:19 -0400

    Constantes
    Signed-off-by: Edson Jr <eds_jr@hotmail.com>

---
+++ Intro/constantes.dart
@@ -0,0 +1,18 @@
+main(){
+	//Criando uma constante  - Tempo de compilacao
+	const pi = 3.14; 
+	//pi = 1; -> vai dar erro
+
+	print('O valor de PI: $pi');  
+
+
+	//constante - em tempo de execucao
+	final edson = new Pessoa();
+	print(edson); //retorna o tipo de instancia
+	
+}
+
+
+class Pessoa {
+	
+}
