e992eb4a972afecdaf8a1b2cb52dd8a217fe47cf
Edson Jr <eds_jr@hotmail.com>
2019-06-24 20:10:17 -0400

    Variaveis booleanas - continuacao
    Signed-off-by: Edson Jr <eds_jr@hotmail.com>

--- Intro/Intellij/variaveis_booleanas/bin/main.dart
+++ Intro/Intellij/variaveis_booleanas/bin/main.dart
@@ -22,7 +22,28 @@ main(List<String> arguments) {
   print('Verdadeiro: ${verdadeiro}');
   print('falso: ${falso}');
 
-  
+  //operador ternario com booleanos
+  bool t1 = false;
+
+  bool result = t1 ? true : false;
+  print('Result: ${result}');
+
+  //outro exemplo  de operador ternario com booleano, agora com cast
+  String r_str = t1 ? 'true' : 'false';
+  print('r_str: ${r_str}');
+
+
+  /*
+  var tzero = 0;
+  if(tzero){
+
+  }
+
+    esse tipo de construcao gera um erro
+   */
+
+
+
 
 
   //Booleano com operador ternario
